74aa80789bc1136cdaae09a31e0e6b5cbe9ce0dc
Amit Verma <akv004@gmail.com>
2021-12-03 15:32:41 -0600

    Update App.tsx
    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./styles.css";
-
+import React from "react";
 //Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, and Neptune.
 const data = [
   {
@@ -29,16 +29,54 @@ const data = [
 ];
 
 export default function App() {
+  const inputRef = React.useRef<any>([]);
+  const [list, setList] = React.useState<any>([]);
+
+  const selectAll = (e: any) => {
+    data.forEach((el, index) => {
+      inputRef.current[index].checked = true;
+    });
+  };
+
+  const clearAll = (e: any) => {
+    data.forEach((el, index) => {
+      inputRef.current[index].checked = false;
+    });
+  };
+
+  const handleChange = () => {
+    const currentSelections = data.map((el, index) => {
+      return inputRef.current[index].checked;
+    });
+    console.log(currentSelections);
+    setList(currentSelections);
+  };
+
   return (
     <div className="App">
       {data.map((el, index) => {
         return (
-          <div className="planet_checkbox">
+          <div key={index} className="planet_checkbox">
             <label className="planet_checkbox-label">{el.name}</label>
-            <input key={el.id} type="checkbox" name={el.name} />
+            <input
+              key={el.id}
+              ref={(e) => (inputRef.current[index] = e)}
+              type="checkbox"
+              name={el.name}
+              onChange={handleChange}
+            />
           </div>
         );
       })}
+      <div>
+        <button onClick={selectAll}>Select All</button>
+      </div>
+
+      <div>
+        <button onClick={clearAll}>Clear All</button>
+      </div>
+å
+      <div>{JSON.stringify({ list })}</div>
     </div>
   );
 }
